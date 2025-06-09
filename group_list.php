265a734eac25961195e106c3238f4095544545bb
JAin <125685354+4928vibe@users.noreply.github.com>
2025-06-09 14:47:51 +0900

    feat: 그룹 카테고리 정렬 및 글쓰기 버튼 추가

--- group_list.php
+++ group_list.php
@@ -3,6 +3,7 @@
     <title>소모임</title>
 </head>
 <body>
+    <form method="POST" action="group_post.php"> <input type="submit" value="소모임 만들기"/> </form>
     <?php
 
         include("db.php");
@@ -14,7 +15,32 @@
             return;
         }
 
-        $sql = "SELECT group_id, title FROM `group` ORDER BY group_id DESC";
+        // 카테고리 선택 필터
+        $selected_category = isset($_GET['category']) ? $_GET['category'] : null;
+
+        // 카테고리 리스트 출력
+        $cat_sql = "SELECT * FROM group_catagories";
+        $cat_result = mysqli_query($conn, $cat_sql);
+
+        echo "<a href='group_list.php'>전체</a>";
+        while ($cat = mysqli_fetch_assoc($cat_result)) {
+            $cat_id = $cat['group_catagory_id'];
+            $cat_name = $cat['name'];
+            echo " | <a href='?category=$cat_id'>$cat_name</a>";
+        }
+        echo "<hr/>";
+
+        if ($selected_category) {
+            $sql = "SELECT group_id, title 
+            FROM `group` 
+            WHERE group_category_id = $selected_category 
+            ORDER BY group_id DESC";
+            } else {
+            $sql = "SELECT group_id, title 
+            FROM `group`
+            ORDER BY group_id DESC";
+            }
+
         $result = mysqli_query($conn, $sql);
 
         echo "<h2>소모임 리스트</h2>";
